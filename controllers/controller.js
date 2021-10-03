0f4e0a92b0c6d3168c10d4212275358b9824331f
Yurii (AnomieRED) <anomiemio@gmail.com>
2021-10-03 21:11:33 +0300

    testing on postamn

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,25 +1,27 @@
+const Users = require('../models/model.users');
+const Role = require('../models/model.role');
+const bcrypt = require('bcryptjs');
+
 class Controller {
 	async registration(req, res) {
 		try {
-
+			const { username, password } = req.body;
+			const hasUser = await Users.findOne({ username });
+			if (hasUser) {
+				return res
+					.status(400)
+					.json({ massage: 'Пользователь с таким именем уже существует' });
+			}
+			const hashPassword = bcrypt.hashSync(password, 7);
+			const userRole = await Role.findOne({ value: 'User' });
+			const createUser = Users.create({
+				username,
+				password: hashPassword,
+				roles: [userRole.value],
+			});
+			return res.json({ message: 'Пользователь успешно зарегистрирован' });
 		} catch (error) {
-
-		}
-	}
-
-	async login(req, res) {
-		try {
-
-		} catch (error) {
-
-		}
-	}
-
-	async getUsers(req, res) {
-		try {
-			res.json('Server worked')
-		} catch (error) {
-
+			res.status(400).json({ message: 'Registaration error' });
 		}
 	}
 }
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('./connection/mongodb');
+const router = require('./routers/router');
 
 const PORT = process.env.SERVER_PORT || 3000;
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use('/auth', router);
 
 app.listen(PORT, () => {
 	console.log(`Server has been started on port ${PORT}`);
