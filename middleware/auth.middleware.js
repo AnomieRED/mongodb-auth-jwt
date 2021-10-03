5d2c159fc1d83ad41d3e9f3de131127c33d6250d
Yurii (AnomieRED) <anomiemio@gmail.com>
2021-10-03 23:41:41 +0300

    created middleware for token auth and role users

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const KEY = process.env.SECRET_KEY;
+
+module.exports = function (req, res, next) {
+	if (req.method === 'OPRIONS') {
+		next();
+	}
+
+	try {
+		const token = req.headers.authorization.split(' ')[1];
+		console.log('Токен из заголовка ', token);
+		if (!token) {
+			return res.status(403).json({ message: 'Пользователь не авторизован' });
+		}
+		const decodedToken = jwt.verify(token, KEY);
+		console.log('Декодированный токен ', decodedToken);
+		req.user = decodedToken;
+		next();
+	} catch (error) {
+		console.log(error);
+		return res.status(403).json({ message: 'Пользователь не авторизован' });
+	}
+};
---
+++ middleware/role.middleware.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const KEY = process.env.SECRET_KEY;
+
+module.exports = function (roles) {
+	return function (req, res, next) {
+		if (req.method === 'OPRIONS') {
+			next();
+		}
+
+		try {
+			const token = req.headers.authorization.split(' ')[1];
+			if (!token) {
+				return res.status(403).json({ message: 'Пользователь не авторизован' });
+			}
+			const { roles: userRoles } = jwt.verify(token, KEY);
+			let hasRole = false;
+			userRoles.forEach((role) => {
+				if (roles.includes(role)) {
+					hasRole = true;
+				}
+			});
+			if (!hasRole) {
+				return res.status(403).json({ message: 'У вас нет доступа' });
+			}
+			next();
+		} catch (error) {
+			console.log(error);
+			return res.status(403).json({ message: 'Пользователь не авторизован' });
+		}
+	};
+};
