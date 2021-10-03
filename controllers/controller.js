b926d525ae0bc8f51f9a6258719d2e8b57c47a30
Yurii (AnomieRED) <anomiemio@gmail.com>
2021-10-03 18:08:48 +0300

    created controllers for auth

---
+++ controllers/controller.js
@@ -0,0 +1,27 @@
+class Controller {
+	async registration(req, res) {
+		try {
+
+		} catch (error) {
+
+		}
+	}
+
+	async login(req, res) {
+		try {
+
+		} catch (error) {
+
+		}
+	}
+
+	async getUsers(req, res) {
+		try {
+			res.json('Server worked')
+		} catch (error) {
+
+		}
+	}
+}
+
+module.exports = new Controller();
